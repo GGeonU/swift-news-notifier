a13168a99503699a52fa6dfdac762eb3d59cdad1
GGeonu <ggeonu0613@gmail.com>
2025-11-08 15:18:43 +0900

    feat: add test code for summary logic to gemini

--- src/summary/summary.module.ts
+++ src/summary/summary.module.ts
@@ -5,6 +5,6 @@ import { SummaryService } from './summary.service';
 @Module({
   controllers: [SummaryController],
   providers: [SummaryService],
-  exports: [SummaryService], // 다른 모듈에서도 SummaryService를 사용할 수 있도록
+  exports: [SummaryService],
 })
 export class SummaryModule {}
