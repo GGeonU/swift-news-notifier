9c1a8728969138e5320f1bb9daebce6f3c0cf9bc
GGeonu <ggeonu0613@gmail.com>
2025-11-14 19:34:14 +0900

    feat: add article fetcher from github

--- src/fetcher/fetcher.module.ts
+++ src/fetcher/fetcher.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FetcherService } from './fetcher.service';
 import { FetcherController } from './fetcher.controller';
+import { SummaryModule } from '../summary/summary.module';
 
 @Module({
+  imports: [SummaryModule], // SummaryModule을 import
   providers: [FetcherService],
   exports: [FetcherService],
-  controllers: [FetcherController], // 다른 모듈에서 사용 가능하도록 export
+  controllers: [FetcherController],
 })
 export class FetcherModule {}
