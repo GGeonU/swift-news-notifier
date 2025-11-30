4e3f65712e713e9227e30215ba1879c0a1499b25
GGeonu <ggeonu0613@gmail.com>
2025-11-30 23:20:10 +0900

    refactor: Improve business logic can be called by events

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -2,11 +2,11 @@ import { Module, forwardRef } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
 import { FetcherModule } from '../fetcher/fetcher.module';
-import { SummaryModule } from '../summary/summary.module';
 
 @Module({
-  // Controller에서는 직접 호출이 필요하므로 forwardRef 사용
-  imports: [forwardRef(() => FetcherModule), forwardRef(() => SummaryModule)],
+  // Controller에서 FetcherService를 직접 호출하므로 FetcherModule만 import
+  // SummaryService는 이벤트 기반으로 자동 연결되므로 불필요
+  imports: [forwardRef(() => FetcherModule)],
   providers: [NotificationService],
   exports: [NotificationService],
   controllers: [NotificationController],
