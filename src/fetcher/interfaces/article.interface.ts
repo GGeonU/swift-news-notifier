6e893e76469783d559b1a3d8940801104e72885f
GGeonu <ggeonu0613@gmail.com>
2025-12-07 16:36:05 +0900

    refactor: cleanup summary service, add test cases

--- src/fetcher/interfaces/article.interface.ts
+++ src/fetcher/interfaces/article.interface.ts
@@ -2,15 +2,7 @@
  * 아티클 데이터 인터페이스
  */
 export interface Article {
-  /** 아티클 제목 */
   title: string;
-
-  /** 아티클 URL */
   url: string;
-
-  /** 발견된 시각 */
   discoveredAt: Date;
-
-  /** 소스 (예: 'github', 'rss' 등) */
-  source?: string;
 }
